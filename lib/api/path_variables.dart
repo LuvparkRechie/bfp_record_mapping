e4004cbc103d4529e845992317f80d6612a1aa87
NATOY <155434447+LuvparkRechie@users.noreply.github.com>
2026-03-25 18:13:35 +0800

    path

--- lib/api/path_variables.dart
+++ lib/api/path_variables.dart
@@ -21,4 +21,5 @@ class ApiKeys {
   static final String reportsList = dotenv.env["REPORTS_LIST"]!;
   static final String generateFsic = dotenv.env["GENERATE_FSIC"]!;
   static final String otpHandler = dotenv.env["OTP_HANDLER"]!;
+  static final String inspectionOrder = dotenv.env["INSPECTION_ORDER"]!;
 }
